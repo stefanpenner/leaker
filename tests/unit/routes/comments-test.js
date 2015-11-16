17ea400c9cb6735a05aa8239daca45cbf3d5a188
edgar calderon <edgar.calderon@fireeye.com>
2015-11-16 13:03:12 -0500

    ember 1.12 compat

--- tests/unit/routes/comments-test.js
+++ tests/unit/routes/comments-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:comments', 'Unit | Route | comments', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['router:main']
 });
 
 test('it exists', function(assert) {
--- tests/unit/routes/posts-test.js
+++ tests/unit/routes/posts-test.js
@@ -3,6 +3,7 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('route:posts', 'Unit | Route | posts', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
+  needs: ['router:main']
 });
 
 test('it exists', function(assert) {
